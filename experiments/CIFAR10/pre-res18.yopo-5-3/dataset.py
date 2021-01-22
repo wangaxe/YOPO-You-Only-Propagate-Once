dd6b4eb9e06e4858a3eaa9d89fd8751c9d72edb8
wangaxe <13633827530@163.com>
2021-01-22 08:10:07 +0000

    0122

--- experiments/CIFAR10/pre-res18.yopo-5-3/dataset.py
+++ experiments/CIFAR10/pre-res18.yopo-5-3/dataset.py
@@ -2,6 +2,10 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import numpy as np
+
+cifar10_mean = (0.4914, 0.4822, 0.4465)
+cifar10_std = (0.2471, 0.2435, 0.2616)
+
 def create_train_dataset(batch_size = 128, root = '../data'):
 
     transform_train = transforms.Compose([
@@ -9,21 +13,23 @@ def create_train_dataset(batch_size = 128, root = '../data'):
      transforms.RandomHorizontalFlip(),
      transforms.ToTensor(),
      #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+     transforms.Normalize(cifar10_mean, cifar10_std),
     ])
 
     trainset = torchvision.datasets.CIFAR10(root=root, train=True, download=True, transform=transform_train)
     #trainset = torchvision.datasets.MNIST(root=root, train=True, download=True, transform=transform_train)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=2)
 
     return trainloader
 def create_test_dataset(batch_size = 128, root = '../data'):
     transform_test = transforms.Compose([
      transforms.ToTensor(),
      #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+     transforms.Normalize(cifar10_mean, cifar10_std),
     ])
     testset = torchvision.datasets.CIFAR10(root=root, train=False, download=True, transform=transform_test)
     #testset = torchvision.datasets.MNIST(root=root, train=False, download=True, transform=transform_test)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, pin_memory=True, num_workers=2)
     return testloader
 
 
